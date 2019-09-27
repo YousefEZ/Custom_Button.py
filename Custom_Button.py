90e135989f1eb5cdf84d050eecae9c242a36779c
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2019-09-27 13:45:25 +0300

    Update Custom_Button.py

--- Custom_Button.py
+++ Custom_Button.py
@@ -5,6 +5,8 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageDraw, ImageFont, ImageTk, features
 import textwrap
+from time import sleep
+from threading import Thread
 
 class Round_Button(tk.Label):
 
@@ -33,57 +35,59 @@ class Round_Button(tk.Label):
         self.multi = multi
         self.aspect_ratio = (int(35*multi), int(10*multi)) # 3.5 : 1 (W : H)
         self.text = text
+        self.change_to_trans = False
+        self.change_to_static = False
 
         self.create_custom_image() #Create static and transformed buttons
-        self.configure(image=self.original_image) #Inserts static button images
+        self.configure(image=self.Images[9]) #Inserts static button images
 
         self.bind("<Enter>", self.on_enter) #Hover on capabilities
         self.bind("<Leave>", self.on_leave) #Hover off capabilities
 
     def create_custom_image(self):
-        self.original_image = Image.new('RGBA', (self.aspect_ratio))
-        self.changed_image = Image.new('RGBA', (self.aspect_ratio))
-
-
-        #Initialising the draw the ImageDraw.Draw object
-        self.original_draw = ImageDraw.Draw(self.original_image)
-        self.changed_draw =  ImageDraw.Draw(self.changed_image)
-
-        #Creates the base for both images (Rectangles)
-        self.original_draw.rectangle((0,0, self.aspect_ratio[0],self.aspect_ratio[1]), fill = self.sc)
-        self.changed_draw.rectangle((0, 0, self.aspect_ratio[0], self.aspect_ratio[1]), fill=self.tc)
-
-        #Create a rectangle to remove the unwanted areas of colour, and adds an elipses to give a round effect.
-        #2 on both sides for 2 images.
-        self.changed_draw.rectangle((self.aspect_ratio[0] - int(5.5*self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=(215, 0, 0, 0))
-        self.changed_draw.ellipse((self.aspect_ratio[0] - int(10*self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=self.tc)
-
-        self.changed_draw.rectangle((0, 0, int(5*self.multi), int(10*self.multi)), fill=(0, 0, 0, 0))
-        self.changed_draw.ellipse((0, 0, int(10*self.multi), int(10*self.multi)), fill=(self.tc))
-
-        self.original_draw.rectangle((self.aspect_ratio[0] - int(5.5*self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=(0, 0, 0, 0))
-        self.original_draw.ellipse((self.aspect_ratio[0] - int(10*self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=self.sc)
-
-        self.original_draw.rectangle((0, 0, int(5.5*self.multi), int(10*self.multi)), fill=(0, 0, 0, 0))
-        self.original_draw.ellipse((0, 0, int(10*self.multi), int(10*self.multi)), fill=self.sc)
 
         decrement = -1
         while True:
-            #< decrement > : Used for lowering the font size so that the text doesn't go off the screen.
+            # < decrement > : Used for lowering the font size so that the text doesn't go off the screen.
             decrement += 1
-            font = ImageFont.truetype("Assets/GentiumBasic-Bold.ttf", int(5.5*self.multi)-decrement, encoding="unic")
-            coords, Lines, line_height = self.draw_multiple_line_text(self.text, font, int(36*self.multi), int(2*self.multi), 12)
+            font = ImageFont.truetype("Assets/GentiumBasic-Bold.ttf", int(5.5 * self.multi) - decrement, encoding="unic")
+            coords, Lines, line_height = self.draw_multiple_line_text(self.text, font, int(36 * self.multi), int(2 * self.multi), 12)
             if coords[-1][1] + line_height + 5 > self.aspect_ratio[1]:
                 continue
-            for i in range (len(coords)):
-                self.original_draw.text(coords[i], Lines[i], fill=self.tsc, font=font, align='center')
-                self.changed_draw.text(coords[i], Lines[i], fill=self.ttc, font=font, align='center')
             break
 
+        self.images = [Image.new('RGBA', (self.aspect_ratio)) for i in range (10)]
 
-        self.original_image = ImageTk.PhotoImage(self.original_image)
-        self.changed_image = ImageTk.PhotoImage(self.changed_image)
+        # Initialising the draw the ImageDraw.Draw object
+        self.image_drawer = [ImageDraw.Draw(self.images[i]) for i in range (10)]
+        self.image_colours = [[self.tc[i] + ((self.sc[i]-self.tc[i])//10)*x for i in range (3)] for x in range (10)]
+        self.text_colours = [[self.ttc[i] + ((self.tsc[i] - self.ttc[i]) // 10) * x for i in range(3)] for x in range(10)]
+        print (self.image_colours)
+        for i in range(10):
 
+            # Puts the colours in a tuple for use.
+            colour = (self.image_colours[i][0],self.image_colours[i][1],self.image_colours[i][2])
+            textcolour = (self.text_colours[i][0], self.text_colours[i][1], self.text_colours[i][2])
+
+            # Creates the base for both images (Rectangles)
+
+            self.image_drawer[i].rectangle((0,0, self.aspect_ratio[0], self.aspect_ratio[1]), fill=colour)
+
+            # Create a rectangle to remove the unwanted areas of colour, and adds an elipses to give a round effect.
+            # 2 on both sides for 2 images.
+
+            self.image_drawer[i].rectangle((self.aspect_ratio[0] - int(5.5 * self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=(0, 0, 0, 0))
+            self.image_drawer[i].ellipse((self.aspect_ratio[0] - int(10 * self.multi), 0, self.aspect_ratio[0], self.aspect_ratio[1]),fill=colour)
+
+            self.image_drawer[i].rectangle((0, 0, int(5 * self.multi), int(10 * self.multi)), fill=(0, 0, 0, 0))
+            self.image_drawer[i].ellipse((0, 0, int(10 * self.multi), int(10 * self.multi)), fill=(colour))
+
+
+
+            for x in range (len(coords)):
+                self.image_drawer[i].text(coords[x], Lines[x], fill=textcolour, font=font, align='center')
+
+        self.Images = [ImageTk.PhotoImage(self.images[i]) for i in range (10)]
 
     def draw_multiple_line_text(self, text, font, text_start_width, text_start_height, Line_Width):
         ## Used for creating multi-line text. Splits the text across multiple lines if the text crosses the line width.
@@ -104,12 +108,44 @@ class Round_Button(tk.Label):
 
     def on_enter(self,*args):
         #switches images to the transformed button.
-        self.configure(image=self.changed_image)
+        print ('Entering')
+        t1 = Thread(target=self.change_sc)
+        t1.start()
+
+    def change_sc(self, si:int=9):
+        self.change_to_static = True
+        for i in range (si,0,-1):
+            if self.change_to_trans == True:
+                self.change_to_static = False
+                self.change_tsc(i)
+                break
+            sleep(0.01)
+            self.configure(image=self.Images[i])
+
+        if self.change_to_static:
+            self.change_to_static = False
+
+    def change_tsc(self, si:int=0):
+
+
+        for i in range (si, 10):
+            if self.change_to_static == True:
+                self.change_to_trans = False
+                self.change_sc(i)
+                break
+            sleep(0.01)
+            self.configure(image=self.Images[i])
+
+        if self.change_to_trans:
+            self.change_to_trans = False
+
 
 
     def on_leave(self,*args):
         #switches back to static image.
-        self.configure(image=self.original_image)
+        print('Leaving')
+        t1 = Thread(target=self.change_tsc)
+        t1.start()
 
     def connect_function(self, function):
         #Binds the button to a function.
@@ -126,15 +162,15 @@ if __name__ == '__main__':
 
     app = tk.Tk()
     Static_Colour = (255,0,0)
-    Text_Static_Colour = (255,255,255)
+    Text_Transformation_Colour = (255,255,255)
     Transformation_Colour = (0,0,255)
-    Text_Transformation_Colour = (0,0,0)
-    Button = [Round_Button(app, 'ExampleTeeset', 7, Static_Colour, Text_Static_Colour, Transformation_Colour, Text_Transformation_Colour) for i in range (4)]
+    Text_Static_Colour = (0,0,0)
+    Button = [Round_Button(app, 'Example', 3, Static_Colour, Text_Static_Colour, Transformation_Colour, Text_Transformation_Colour) for i in range (4)]
 
     [Button[x].connect_function(New_Function) for x in range (4)]
 
     Button[0].grid(row=0, column=0)
-    Button[1].grid(row=0, column=1)
-    Button[2].grid(row=1, column=0)
-    Button[3].grid(row=1, column=1)
+    #Button[1].grid(row=0, column=1)
+    #Button[2].grid(row=1, column=0)
+    #Button[3].grid(row=1, column=1)
     app.mainloop()
